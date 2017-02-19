d2a1a472b2bb17ff501120c2a8801c490da79a17
ramyyahari <ramyya@gmail.com>
2017-02-18 17:41:11 -0800

    Delete gulpfile.js

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var isTravis = process.env.TRAVIS || false;
-var babel = require('gulp-babel'); 
-
-gulp.task('travis', ['build', 'testServerJS'], function() {
-	process.exit(0);
-});
