88789006f94a1f32aaed65ed20cb883e4c4171ee
Ramyya <ramyya@gmail.com>
2017-03-12 22:49:27 -0700

    Added drap-and drop selection

--- src/pages/NoteBook.js
+++ src/pages/NoteBook.js
@@ -2,32 +2,29 @@ import React from 'react';
 import { Link } from 'react-router';
 import DocumentTitle from 'react-document-title';
 import Input from 'muicss/lib/react/input';
-
+import Dropzone from 'react-dropzone';
 
 export default class NoteBook extends React.Component {
  
-  constructor() {
-     super();
-     this.state = {
-        content:'', 
-       };
-  } 
+  // constructor() {
+  //    super();
+  //    this.state = {
+  //       content:'', 
+  //      };
+  // } 
    
-  handleChange(name, e) {
-    var change = {};
-    change[name] = e.target.value;
-    this.setState(change);
-    console.log("in toggle "+e.target.value);
+  onDrop(acceptedFiles, rejectedFiles) {
+      console.log('Accepted files: ', acceptedFiles);
+      console.log('Rejected files: ', rejectedFiles);
   }
 
   render() {
-    var total = this.state.content;
-      return (
-        <div>
-          <h3>{total}<br/>
-            <Input hint="New element" value={this.state.content} onChange={this.handleChange.bind(this, 'content')} />
-          </h3>
-        </div>
-      );
+    return (
+      <div>
+        <Dropzone onDrop={this.onDrop}>
+          <div>Try dropping some files here, or click to select files to upload.</div>
+        </Dropzone>
+      </div>
+    );
  }
 }
