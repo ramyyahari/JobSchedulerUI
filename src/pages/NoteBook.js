e4a6d4d0f4fde04e14c3a424986ae51248d7bf08
Ramyya <ramyya@gmail.com>
2017-03-20 17:43:28 -0700

    Removed appbar

--- src/pages/NoteBook.js
+++ src/pages/NoteBook.js
@@ -14,8 +14,7 @@ var styles = {
   },
   tabItems:{
     backgroundColor: 'indigo',
-    width: 'auto',
-    padding: '8'
+    
   }
 };
 
@@ -45,19 +44,14 @@ onFocus(value) {
     return (
         <MuiThemeProvider>   
           <div>
-            <AppBar title="Portfolio"  
-              titleStyle={{ width: 'auto'}}    
-              showMenuIconButton={false} 
-              style={styles.appBar}>    
               <Tabs value={this.state.selected} tabItemContainerStyle={styles.tabItems}>
-                <Tab label=" Item One " style={{ width:'auto', padding: '8'}} value={0} />
-                <Tab label=" Item two " style={{ width: 'auto' , padding: '8'}} value={1} />
-                <Tab label=" Item three " style={{ width: 'auto', padding: '8'}} value={2}/>
-                <Tab label=" Item four " style={{ width: 'auto', padding: '8'}} value={3}/>
-                <Tab label=" Item five " style={{ width: 'auto', padding: '8'}} value={4}/>
-                <Tab label=" Item six " style={{ width: 'auto', padding: '8'}} value={5}/>
+                <Tab label=" Item One "  value={0} />
+                <Tab label=" Item two "  value={1} />
+                <Tab label=" Item three " value={2}/>
+                <Tab label=" Item four "  value={3}/>
+                <Tab label=" Item five "  value={4}/>
+                <Tab label=" Item six "  value={5}/>
               </Tabs>    
-            </AppBar> 
         <List>
             <ListItem initiallyOpen={true} disabled={true} onMouseOver={ (e) => {this.onFocus(2);} } primaryText={<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>} /> 
             <ListItem initiallyOpen={true} disabled={true} primaryText={<p>2Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>} /> 
