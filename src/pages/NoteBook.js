b43ac50f77697850d0ec2a3bf09a10967a0b26fe
Ramyya <ramyya@gmail.com>
2017-03-20 22:44:03 -0700

    Notebook template for portfolio

--- src/pages/NoteBook.js
+++ src/pages/NoteBook.js
@@ -7,12 +7,8 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 import {List, ListItem} from 'material-ui/List';
 
 var styles = {
-  appBar: {
-    flexWrap: 'wrap',
-    backgroundColor: 'indigo',
-    position: 'fixed',
-  },
   tabItems:{
+    float: 'left',
     backgroundColor: 'indigo',
     
   }
@@ -46,11 +42,9 @@ onFocus(value) {
           <div>
               <Tabs value={this.state.selected} tabItemContainerStyle={styles.tabItems}>
                 <Tab label=" Item One "  value={0} />
-                <Tab label=" Item two "  value={1} />
+                <Tab label=" Item two " value={1} />
                 <Tab label=" Item three " value={2}/>
                 <Tab label=" Item four "  value={3}/>
-                <Tab label=" Item five "  value={4}/>
-                <Tab label=" Item six "  value={5}/>
               </Tabs>    
         <List>
             <ListItem initiallyOpen={true} disabled={true} onMouseOver={ (e) => {this.onFocus(2);} } primaryText={<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>} /> 
