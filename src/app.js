c2ce83547c6acfd98f97abc3afb70db41c9c10a2
ADlab <ramyya@gmail.com>
2017-06-01 17:36:10 -0700

    Modifying routes

--- src/app.js
+++ src/app.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { IndexRoute, Route, browserHistory } from 'react-router';
+import { Router, HomeRoute, IndexRoute, Route, browserHistory } from 'react-router';
 import { JobSubmission, NoteBook, JobForm, Toggle, MasterPage, IndexPage } from './pages';
 import { LoginPage, RegistrationPage, ProfilePage } from './User';
-
-
-ReactStormpath.init();
 
 ReactDOM.render(
 	<Router history={browserHistory}>
   	<HomeRoute path='/' component={MasterPage}>
-  		<IndexRoute component={IndexPage} />
-  		<LoginRoute path='/login' component={LoginPage} />
+  		<IndexRoute path='/login' component={LoginPage} />
   		<Route path='/register' component={RegistrationPage} />
   		<Route path='/profile' component={ProfilePage} />
   		<Route path='/jobform' component={JobForm} />      
       <Route path='/jobsubmission' component={JobSubmission} />      
       <Route path='/notebook' component={NoteBook} />   
-      <Route path='/logout' component={LogoutLink} />    
       <Route path='/toggle' component={Toggle} />
-      <AuthenticatedRoute>	
-        <HomeRoute path='/' component={IndexPage} />
-      </AuthenticatedRoute>	
-  	</HomeRoute>
+    </HomeRoute>
 	</Router>,
   	document.getElementById('app-container')
 );
